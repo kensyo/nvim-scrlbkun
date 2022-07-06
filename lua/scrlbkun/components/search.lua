494a36f5d82e0b304826891ee81073f1bdc1d1e3
kensyo <takitateokowa@gmail.com>
2022-07-06 17:06:54 +0900

    [FIX] Absorb an error when specifying an invalid regular expression pattern is specified
    Stop using low level functions by ffi and use neovim's search()
    function.

--- lua/scrlbkun/components/search.lua
+++ lua/scrlbkun/components/search.lua
@@ -1,6 +1,4 @@
-local ffi = require('ffi')
 local util= require('scrlbkun.util')
-local C = ffi.C
 local states = require('scrlbkun.states')
 
 local api = vim.api
@@ -50,95 +48,6 @@ local built_in_signs = {
     {"▔", "▔", "▀", "▀", "▀", "▀", "█", "█"},
 }
 
-----------------------------------------------------------------------------------------------
-----------------------------------------------------------------------------------------------
-
--- The argument of ffi.cdef, C_char_u_VLA, C_regmatch_T, get_win_T, get_buf_T, and build_regmatch_T
--- are the codes obatained by mofifying the codes in
---     * https://github.com/kevinhwang91/nvim-hlslens/blob/main/lua/hlslens/wffi.lua
--- .
--- Origiinally under BSD 3-Clause lisence, https://github.com/kevinhwang91/nvim-hlslens/blob/main/LICENSE
---   Copyright (c) 2021, kevinhwang91
--- Relicensed under the MIT license, https://github.com/kensyo/nvim-scrlbkun/blob/main/LICENSE
---   Copyright (c) 2022, kensyo
-
-ffi.cdef([[
-    typedef unsigned char char_u;
-    typedef struct regprog regprog_T;
-
-    typedef long linenr_T;
-    typedef int colnr_T;
-
-    typedef uint64_t proftime_T;
-
-    typedef struct {
-        linenr_T lnum;
-        colnr_T col;
-    } lpos_T;
-
-    typedef struct {
-        regprog_T *regprog;
-        lpos_T startpos[10];
-        lpos_T endpos[10];
-        int rmm_ic;
-        colnr_T rmm_maxcol;
-    } regmmatch_T;
-
-    typedef struct window_S win_T;
-    typedef struct file_buffer buf_T;
-
-    regprog_T *vim_regcomp(char_u *expr_arg, int re_flags);
-    void vim_regfree(regprog_T *prog);
-
-    char_u *ml_get_buf(buf_T *buf, linenr_T lnum, bool will_change);
-
-    int ignorecase(char_u *pat);
-
-    long vim_regexec_multi(regmmatch_T *rmp, win_T *win, buf_T *buf, linenr_T lnum, colnr_T col,
-        proftime_T *tm, int *timed_out);
-
-    size_t strlen(const char *s);
-
-    int curwin_col_off(void);
-
-    typedef struct {} Error;
-    buf_T *find_buffer_by_handle(int buffer, Error *err);
-    win_T *find_window_by_handle(int window, Error *err);
-]])
-
-local C_char_u_VLA = ffi.typeof('char_u[?]')
-local C_regmmatch_T = ffi.typeof('regmmatch_T')
-
-local function get_win_T(window_id)
-    -- local err = ffi.new('Error')
-    local win_T = C.find_window_by_handle(window_id, nil)
-    return win_T
-end
-
-local function get_buf_T(buffer_number)
-    -- local err = ffi.new('Error')
-    local buf_T = C.find_buffer_by_handle(buffer_number, nil)
-    return buf_T
-end
-
-local function build_regmatch_T(pat)
-    C_pattern = C_char_u_VLA(#pat + 1)
-    ffi.copy(C_pattern, pat)
-
-    local reg_prog = C.vim_regcomp(C_pattern, vim.o.magic and 1 or 0)
-    if reg_prog == nil then
-        return
-    end
-    local regm = C_regmmatch_T()
-    regm.regprog = reg_prog
-    regm.rmm_ic = C.ignorecase(C_pattern)
-    regm.rmm_maxcol = 0
-    return regm
-end
-
-----------------------------------------------------------------------------------------------
-----------------------------------------------------------------------------------------------
-
 local M = require('scrlbkun.components.base').new(component_name, default_config, highlights)
 
 local function is_search_mode()
@@ -175,41 +84,40 @@ function M:calculate(window_id)
 
         match_result = cache[buffer_number].match_result
     else
-        local reg_match = build_regmatch_T(pattern)
-        local win_T = get_win_T(window_id)
-        local buf_T = get_buf_T(buffer_number)
+        local original_cursor_position = api.nvim_win_get_cursor(window_id)
+        api.nvim_win_set_cursor(window_id, {1, 0})
+        local line_number = 1
+        while true do
+            local ok
+            ok, line_number = pcall(vim.fn.search, pattern, "W")
+            if not ok then
+                return {}
+            end
 
-        for line_number = 1, number_of_lines_in_buffer do
-            if C.vim_regexec_multi(
-                    reg_match,
-                    win_T,
-                    buf_T,
-                    line_number,
-                    0,
-                    nil,
-                    nil
-                ) >= 1 then
-                -- 0-based
-                local strict_coordinate = (line_number - 1) / number_of_lines_per_row
-                local coordinate = math.floor(strict_coordinate)
-                local fractional_part = strict_coordinate - coordinate
-                if match_result[coordinate] then
-                    if fractional_part < 0.5 then
-                        match_result[coordinate].upper = match_result[coordinate].upper + 1
-                    else
-                        match_result[coordinate].lower = match_result[coordinate].lower + 1
-                    end
+            if line_number == 0 then
+                -- The behavior doens't change without the below line. why?
+                api.nvim_win_set_cursor(window_id, original_cursor_position)
+                break
+            end
+
+            local strict_coordinate = (line_number - 1) / number_of_lines_per_row
+            local coordinate = math.floor(strict_coordinate)
+            local fractional_part = strict_coordinate - coordinate
+            if match_result[coordinate] then
+                if fractional_part < 0.5 then
+                    match_result[coordinate].upper = match_result[coordinate].upper + 1
                 else
-                    match_result[coordinate] = {upper = 0, lower = 0}
-                    if fractional_part < 0.5 then
-                        match_result[coordinate].upper = 1
-                    else
-                        match_result[coordinate].lower = 1
-                    end
+                    match_result[coordinate].lower = match_result[coordinate].lower + 1
+                end
+            else
+                match_result[coordinate] = {upper = 0, lower = 0}
+                if fractional_part < 0.5 then
+                    match_result[coordinate].upper = 1
+                else
+                    match_result[coordinate].lower = 1
                 end
             end
         end
-        C.vim_regfree(reg_match.regprog)
 
         -- Update cache
         cache[buffer_number] = {
